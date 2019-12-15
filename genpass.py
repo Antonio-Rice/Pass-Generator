5f4ec4ef4af0a94f8dc680f5a208794e176b6a2a
AntonioRice <58892473+Antonio-Rice@users.noreply.github.com>
2019-12-15 13:30:25 +0100

    Update genpass.py

--- genpass.py
+++ genpass.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 import random
-longueur = input("Longueur du mot de passe ? ")
-longueur = longueur-1
+import sys
+for arg in sys.argv: 1
+aide = """
+Usage: python genpass.py <number of characters>
+
+Example usage:
+    python genpass.py 8
+"""
+while True:
+	try:
+		int(arg)
+		break
+	except ValueError:
+		print(aide)
+		sys.exit(1)
 lettre = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!?+-_:;[]%#$&@")
 n = 0
 mdp = random.choice(lettre)
-while n < longueur:
+arg = int(arg)-1
+while n < int(arg):
 	mdp = mdp+random.choice(lettre)
 	n = n+1
 print (mdp)
