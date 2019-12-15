49ea5df3fb93fbbb4409e5b22c3ae1e2a345f84b
AntonioRice <58892473+Antonio-Rice@users.noreply.github.com>
2019-12-15 11:37:22 +0100

    Update genpass.py
    Ajout de caractères spéciaux pouvant être utilisés pour votre mot de passe
    Adding special characters that can be used for your password

--- genpass.py
+++ genpass.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
 import random
 longueur = input("Longueur du mot de passe ? ")
 longueur = longueur-1
-lettre = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789&@$")
+lettre = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!?+-_:;[]%#$&@")
 n = 0
 mdp = random.choice(lettre)
 while n < longueur:
