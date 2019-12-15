b657e00b0e603f86a5f0006409d498145c5177e7
Antonio-Rice <58892473+Antonio-Rice@users.noreply.github.com>
2019-12-15 10:55:12 +0100

    Add files via upload

---
+++ genpass.py
@@ -0,0 +1,10 @@
+import random
+longueur = input("Longueur du mot de passe ? ")
+longueur = longueur-1
+lettre = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789&@$")
+n = 0
+mdp = random.choice(lettre)
+while n < longueur:
+	mdp = mdp+random.choice(lettre)
+	n = n+1
+print (mdp)
